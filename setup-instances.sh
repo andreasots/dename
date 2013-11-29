1f4cf638451a14b1134e173cb8aa527fdf3c366c
Daniel Ziegler <dmz@mit.edu>
2013-11-29 16:09:52 -0500

    scripts: a bit of cleanup

--- setup-instances.sh
+++ setup-instances.sh
@@ -10,33 +10,16 @@ fi
 dir=$1
 count=$2
 
-tmp_config=`mktemp`
-cp dename.cfg $tmp_config
-
-config=$(cat $tmp_config)
-
-re='\[general\].*^host = ([0-9.i$]+)'
-[[ $config =~ $re ]]
-host_pattern=${BASH_REMATCH[1]}
-
-re='\[database\].*^name = ([-a-zA-Z0-9$]+)'
-[[ $config =~ $re ]]
-dbname_pattern=${BASH_REMATCH[1]}
-
-re='\[database\].*^username = ([-a-zA-Z0-9$]+)'
-[[ $config =~ $re ]]
-dbuser_pattern=${BASH_REMATCH[1]}
-
-re='\[database\].*^password = ([-a-zA-Z0-9$]+)'
-[[ $config =~ $re ]]
-dbpassword_pattern=${BASH_REMATCH[1]}
+tmp_peer_config=`mktemp`
 
 setup_instance() {
 	mkdir $dir/$1
-	host=$(echo $host_pattern | sed "s/\$1/$((($i+43)))/g")
-	dbname=$(eval echo $dbname_pattern)
-	dbuser=$(eval echo $dbuser_pattern)
-	dbpassword=$(eval echo $dbuser_pattern)
+	config=$(cat dename.cfg | sed -e "s/\$2/$((($1+43)))/g" -e "s/\$1/$1/g")
+	echo "$config" > $dir/$1/dename.cfg
+	host=$(grep -Pzo '\[general\]\n(.+\n)*host\s*=\s*\K.+' $dir/$1/dename.cfg)
+	dbname=$(grep -Pzo '\[database\]\n(.+\n)*name\s*=\s*\K.+' $dir/$1/dename.cfg)
+	dbuser=$(grep -Pzo '\[database\]\n(.+\n)*username\s*=\s*\K.+' $dir/$1/dename.cfg)
+	dbpassword=$(grep -Pzo '\[database\](.+\n)+password\s*=\s*\K.+' $dir/$1/dename.cfg)
 	sudo ip addr add $host/30 dev lo:$1
 	cmd="
 CREATE ROLE \"$dbuser\" WITH LOGIN PASSWORD '$dbpassword';
@@ -47,7 +30,7 @@ CREATE ROLE \"$dbuser\" WITH LOGIN PASSWORD '$dbpassword';
 	pk=$(go run $GOPATH/src/github.com/andres-erbsen/sgp/keygen/keygen.go 2>$dir/$1/sk | base64 | tr -d '\n')
 	echo "
 [peer \"$pk\"]
-host = $host" >> $tmp_config
+host = $host" >> $tmp_peer_config
 }
 mkdir $dir
 for i in $(seq 1 $count)
@@ -56,7 +39,5 @@ do
 done
 for i in $(seq 1 $count)
 do
-	host=$(echo $host_pattern | sed "s/\$1/$((($i+43)))/g")
-
-	sed -e "s/$host_pattern/$host/g" -e "s/\$1/$i/g" $tmp_config > $dir/$i/dename.cfg
+	cat $tmp_peer_config >> $dir/$i/dename.cfg
 done
