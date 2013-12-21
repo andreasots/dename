c041ec0c3d7eeaf69a976c1b91ae86f4472903d3
Andres Erbsen <andres@krutt.org>
2013-12-21 02:48:44 +0200

    pseudorandom number generator

---
+++ prng/prng.go
@@ -0,0 +1,73 @@
+package prng
+
+import (
+	"code.google.com/p/go.crypto/salsa20/salsa"
+	"sync"
+	// FIXME: go.crypto/salsa20/salsa.core is not exported, workaround:
+	"github.com/dchest/nacl/salsa20"
+)
+
+const BLOCKSIZE = 64
+
+// PRNG implements a cryptographically secure random number generator that
+// exposes both io.Reader and math/rand.Source API. At the moment, salsa20
+// keystream is used. It is safe to use concurrently from multiple goroutines.
+type PRNG struct {
+	key       [32]byte
+	counter   [16]byte
+	block     [64]byte
+	remaining int
+	sync.Mutex
+}
+
+func newPRNG(key *[32]byte) *PRNG {
+	ret := new(PRNG)
+	copy(ret.key[:], key[:])
+	return ret
+}
+
+func (s *PRNG) Read(b []byte) (int, error) {
+	s.Lock()
+	defer s.Unlock()
+	n := len(b)
+	for len(b) > 0 {
+		c := s.remaining
+		if len(b) < c {
+			c = len(b)
+		}
+		copy(b[:c], s.block[BLOCKSIZE-s.remaining:])
+		b = b[c:]
+		s.remaining -= c
+		if s.remaining <= 0 {
+			salsa20.Core(&s.block, &s.counter, &s.key, &salsa.Sigma)
+			s.remaining = BLOCKSIZE
+			// Add one to the little-endian 16-byte counter
+			u := uint32(1)
+			for i := 8; i < 16; i++ {
+				u += uint32(s.counter[i])
+				s.counter[i] = byte(u)
+				u >>= 8
+			}
+		}
+	}
+	return n, nil
+}
+
+// Int63 returns a non-negative pseudo-random 63-bit integer as an int64
+func (s *PRNG) Int63() int64 {
+	var bs [8]byte
+	s.Read(bs[:])
+	ret := int64(bs[0]&0x7f) << (8 * 7)
+	ret |= int64(bs[1]) << (8 * 6)
+	ret |= int64(bs[2]) << (8 * 5)
+	ret |= int64(bs[3]) << (8 * 4)
+	ret |= int64(bs[4]) << (8 * 3)
+	ret |= int64(bs[5]) << (8 * 2)
+	ret |= int64(bs[6]) << (8 * 1)
+	ret |= int64(bs[7])
+	return ret
+}
+
+func (s *PRNG) Seed(int64) {
+	panic("Cannot seed a cryptographic PRNG with an int")
+}
---
+++ prng/prng_test.go
@@ -0,0 +1,85 @@
+package prng
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"math/rand"
+	"testing"
+)
+
+func fromHex(s string) []byte {
+	ret, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+// from https://github.com/AlexWebr/salsa20/blob/master/test_vectors.256
+var testVectors = []struct {
+	key  []byte
+	out  []byte // bytes 0..63
+	out2 []byte // bytes 192..255
+}{
+	{
+		fromHex("8000000000000000000000000000000000000000000000000000000000000000"),
+		fromHex("E3BE8FDD8BECA2E3EA8EF9475B29A6E7003951E1097A5C38D23B7A5FAD9F6844B22C97559E2723C7CBBD3FE4FC8D9A0744652A83E72A9C461876AF4D7EF1A117"),
+		fromHex("57BE81F47B17D9AE7C4FF15429A73E10ACF250ED3A90A93C711308A74C6216A9ED84CD126DA7F28E8ABF8BB63517E1CA98E712F4FB2E1A6AED9FDC73291FAA17"),
+	},
+	{
+		fromHex("0040000000000000000000000000000000000000000000000000000000000000"),
+		fromHex("01F191C3A1F2CC6EBED78095A05E062E1228154AF6BAE80A0E1A61DF2AE15FBCC37286440F66780761413F23B0C2C9E4678C628C5E7FB48C6EC1D82D47117D9F"),
+		fromHex("86D6F824D58012A14A19858CFE137D768E77597B96A4285D6B65D88A7F1A87784BF1A3E44FC9D3525DDC784F5D99BA222712420181CABAB00C4B91AAEDFF521C"),
+	},
+	{
+		fromHex("0000200000000000000000000000000000000000000000000000000000000000"),
+		fromHex("C29BA0DA9EBEBFACDEBBDD1D16E5F5987E1CB12E9083D437EAAAA4BA0CDC909E53D052AC387D86ACDA8D956BA9E6F6543065F6912A7DF710B4B57F27809BAFE3"),
+		fromHex("77DE29C19136852CC5DF78B5903CAC7B8C91345350CF97529D90F18055ECB75AC86A922B2BD3BD1DE3E2FB6DF915316609BDBAB298B37EA0C5ECD917788E2216"),
+	},
+}
+
+func TestSalsa20PRNGTestVectors(t *testing.T) {
+	var key [32]byte
+	for i, test := range testVectors {
+		copy(key[:], test.key)
+		out := make([]byte, len(test.out))
+		s := newPRNG(&key)
+		s.Read(out)
+		if !bytes.Equal(out, test.out) {
+			t.Errorf("#%d: bad result", i)
+		}
+		s.Read(out)
+		s.Read(out)
+		s.Read(out)
+		if !bytes.Equal(out, test.out2) {
+			t.Errorf("#%d: bad result", i)
+		}
+	}
+}
+
+func TestSalsa20PRNGInt63(t *testing.T) {
+	var key [32]byte
+	for i, test := range testVectors {
+		copy(key[:], test.key)
+		var correct int64
+		binary.Read(bytes.NewBuffer(test.out[:8]), binary.BigEndian, &correct)
+		if correct >= 0 {
+			if newPRNG(&key).Int63() != correct {
+				t.Errorf("#%d: bad result", i)
+			}
+		}
+	}
+}
+
+func TestSalsa20PRNGMathRand(t *testing.T) {
+	var key [32]byte
+	for i, test := range testVectors {
+		copy(key[:], test.key)
+		r := rand.New(newPRNG(&key))
+		x := r.Float64()
+		if x < 0 || x >= 1 {
+			t.Errorf("#%d: bad result", i)
+		}
+	}
+}
