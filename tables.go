caade29bc4a1763bbc7487a9f3847f4527faf94b
Andres Erbsen <andres@krutt.org>
2014-01-08 03:15:02 +0200

    new table layout

--- tables.go
+++ tables.go
@@ -6,6 +6,7 @@ import (
 
 func (dn *Dename) CreateTables() {
 	db := dn.db
+	// === general consensus === //
 	// servers
 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS servers (
 		id serial not null primary key);`)
@@ -16,82 +17,44 @@ func (dn *Dename) CreateTables() {
 	// rounds
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS rounds (
 		id bigserial not null primary key,
-		end_time bigint not null,
-		commit_time bigint,
-		signed_root bytea,
-		naming_snapshot bigint);`)
+		snapshot_number bigint,
+		snapshot_hash bytea);`)
 	if err != nil {
 		log.Fatal("Cannot create table rounds: ", err)
 	}
 
-	// round_keys
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS round_keys (
+	// messages
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS messages (
 		id bigserial not null primary key,
 		round bigint not null,
-		server integer not null,
-		key bytea unique not null,
+		type integer not null,
+		from integer not null,
+		to integer not null,
+		message bytea unique not null,
 		FOREIGN KEY(round) REFERENCES rounds(id),
-		FOREIGN KEY(server) REFERENCES servers(id));`)
-	if err != nil {
-		log.Fatal("Cannot create table round_keys: ", err)
-	}
-
-	// round_signatures
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS round_signatures (
-		id bigserial not null primary key,
-		round bigint not null,
-		server integer not null,
-		signature bytea unique not null,
-		FOREIGN KEY(round) REFERENCES rounds(id),
-		FOREIGN KEY(server) REFERENCES servers(id));`)
-	if err != nil {
-		log.Fatal("Cannot create table round_signatures: ", err)
-	}
-
-	// names_we_transfer
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS names_we_transfer (
-		id bigserial not null primary key,
-		round bigint not null,
-		name bytea not null,
-		FOREIGN KEY(round) REFERENCES rounds(id));`)
-	if err != nil {
-		log.Fatal("Cannot create table names_we_transfer: ", err)
-	}
-
-	// transaction_queue
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS transaction_queue (
-		id bigserial not null primary key,
-		round bigint not null,
-		introducer integer not null,
-		request bytea unique not null,
-		FOREIGN KEY(round) REFERENCES rounds(id),
-		FOREIGN KEY(introducer) REFERENCES servers(id));`)
-	if err != nil {
-		log.Fatal("Cannot create table transaction_queue: ", err)
-	}
-
-	// commitments
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS commitments (
-		id bigserial not null primary key,
-		round bigint not null,
-		commiter integer not null,
-		acknowledger integer not null,
-		signature bytea unique not null,
-		FOREIGN KEY(round) REFERENCES rounds(id),
-		FOREIGN KEY(commiter) REFERENCES servers(id),
-		FOREIGN KEY(acknowledger) REFERENCES servers(id));`)
+		FOREIGN KEY(from) REFERENCES servers(id),
+		FOREIGN KEY(to) REFERENCES servers(id));`)
 	if err != nil {
 		log.Fatal("Cannot create table commitments: ", err)
 	}
 
+	// === naming === //
 	// name_mapping
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS name_mapping (
-		id bigserial not null primary key,
-		name bytea not null,
-		pubkey bytea not null,
-		last_modified bigint not null,
-		FOREIGN KEY(last_modified) REFERENCES rounds(id));`)
+		name bytea not null primary key,
+		pubkey bytea not null;`)
 	if err != nil {
 		log.Fatal("Cannot create table name_mapping: ", err)
 	}
+
+	// name_locked
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS name_locked (
+		round bigint not null,
+		name bytea not null,
+		PRIMARY KEY(round, name),
+		FOREIGN KEY(round) REFERENCES rounds(id));`)
+	if err != nil {
+		log.Fatal("Cannot create table name_locked: ", err)
+	}
+
 }
