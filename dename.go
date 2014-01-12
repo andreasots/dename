2ba64cf18157ccf74016eeb89b07eeffb3a433e8
Andres Erbsen <andres@krutt.org>
2014-01-12 21:19:19 +0200

    Request handling (1/?)

--- dename.go
+++ dename.go
@@ -3,15 +3,19 @@ package main
 import (
 	"bytes"
 	"code.google.com/p/gcfg"
+	"code.google.com/p/goprotobuf/proto"
 	"database/sql"
 	"encoding/base64"
 	"fmt"
 	"github.com/andres-erbsen/dename/consensus"
+	"github.com/andres-erbsen/dename/pgutil"
 	"github.com/andres-erbsen/dename/prng"
 	"github.com/andres-erbsen/dename/protocol"
 	"github.com/andres-erbsen/sgp"
 	"github.com/daniel-ziegler/merklemap"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
 	"os"
 	"os/signal"
@@ -127,23 +131,156 @@ func main() {
 		}
 	}
 
-	peers := make(map[int64]consensus.Peer_, len(dn.peers))
+	consensus_peers := make(map[int64]consensus.Peer_, len(dn.peers))
 	for k, v := range dn.peers {
-		peers[k] = v
+		consensus_peers[k] = v
 	}
-	dn.c = consensus.NewConsensus(dn.db, &dn.our_sk, dn.us.id,
-		func(rqs map[int64]*[][]byte, _ *prng.PRNG) []byte {
-			log.Printf("Requests this round:")
-			for k, v := range rqs {
-				log.Printf(" %v;%v", k, *v)
-			}
-			return []byte("HASH1234HASH5678HASH8765HASH4321")
-		},
-		time.Now(), 4*time.Second, peers, protocol.ConsensusSignTags)
-	go dn.c.Run()
+	dn.c = consensus.NewConsensus(dn.db, &dn.our_sk, dn.us.id, dn.QueueProcessor,
+		time.Now(), 4*time.Second, consensus_peers, protocol.ConsensusSignTags)
 
 	go dn.ListenForPeers()
 	go dn.ConnectToPeers()
 	go dn.ListenForClients()
-	select {}
+	dn.c.Run()
+}
+
+func (dn *Dename) HandleClient(conn net.Conn) {
+	defer conn.Close()
+	msg_bs, err := ioutil.ReadAll(conn)
+	if err != nil {
+		return
+	}
+	msg := new(protocol.C2SMessage)
+	if err = proto.Unmarshal(msg_bs, msg); err != nil {
+		return
+	}
+
+	switch {
+	case msg.TransferName != nil:
+		dn.HandleClientTransfer(msg.TransferName)
+	case msg.Lookup != nil:
+		// TODO: dn.HandleLookup(conn, *msg.Lookup)
+	}
+}
+
+func (dn *Dename) HandleClientTransfer(rq_bs []byte) {
+	name, _, err := dn.ValidateRequest(rq_bs)
+	if err != nil {
+		return
+	}
+	dn.c.IncomingRequests <- rq_bs
+	_, err = dn.db.Exec(`INSERT INTO name_locked(name,request) VALUES($1,$2)`,
+		name, rq_bs)
+	if err == nil {
+		dn.c.IncomingRequests <- rq_bs
+	} else if pgutil.IsError(err, pgutil.ErrUniqueViolation) {
+		return // could not acquire lock
+	} else {
+		log.Fatalf("HandleClientTransfer: Lock a name: %s", err)
+	}
+}
+
+func (dn *Dename) ValidateRequest(rq_bs []byte) (name []byte, new_pk *sgp.Entity, err error) {
+	rq_signed := &sgp.Signed{}
+	if err = proto.Unmarshal(rq_bs, rq_signed); err != nil {
+		return
+	}
+	transfer := &protocol.TransferName{}
+	if err = proto.Unmarshal(rq_signed.Message, transfer); err != nil {
+		return
+	}
+	new_pk = new(sgp.Entity)
+	if err = new_pk.Parse(transfer.PublicKey); err != nil {
+		return
+	}
+
+	var old_pk_bs []byte
+	var old_pk *sgp.Entity
+	err = dn.db.QueryRow("SELECT pubkey FROM name_mapping WHERE name = $1",
+		transfer.Name).Scan(&old_pk_bs)
+	if err == nil { // name already in use; transfer
+		old_pk = new(sgp.Entity)
+		if err := old_pk.Parse(old_pk_bs); err != nil {
+			log.Fatalf("Bad pk in database: %s", err)
+		}
+	} else if err == sql.ErrNoRows { // new name; registration
+		old_pk = new_pk
+	} else { // barf
+		log.Fatalf("Load old pk from db: %s", err)
+	}
+
+	if _, err = old_pk.Verify(rq_bs, protocol.SIGN_TAG_TRANSFER); err != nil {
+		return nil, nil, err
+	}
+	// TODO: require a proof of freshnesh of the request
+	return
+}
+
+func (dn *Dename) QueueProcessor(peer_rq_map map[int64]*[][]byte,
+	shared_prng *prng.PRNG) []byte {
+	var last_snapshot int64 // TODO: load last_snapshot
+	// everything here must be idempotent as this function may be run multiple times
+	mapHandle, err := dn.merklemap.GetSnapshot(last_snapshot).OpenHandle()
+	if err != nil {
+		log.Fatalf("dn.merklemap.GetSnapshot(last_snapshot).OpenHandle(): %s", err)
+	}
+
+	create, err := dn.db.Prepare(`INSERT INTO name_mapping(name) VALUES($1)`)
+	if err != nil {
+		log.Fatalf("PREPARE: INSERT name INTO name_mapping: %s", err)
+	}
+	defer create.Close()
+
+	assign, err := dn.db.Prepare(`UPDATE name_mapping
+		SET pubkey = $1 WHERE name = $2`)
+	if err != nil {
+		log.Fatalf("PREPARE: UPDATE pubkey WERE name: %s", err)
+	}
+	defer assign.Close()
+
+	unlock, err := dn.db.Prepare(`DELETE FROM name_locked
+		WHERE name = $1 AND request = $2`)
+	if err != nil {
+		log.Fatalf("PREPARE: DELETE FROM name_locked WHERE name = $1: %s", err)
+	}
+	defer unlock.Close()
+
+	name_modified := make(map[string]int)
+	for peer_id := range rand.New(shared_prng).Perm(len(peer_rq_map)) {
+		for _, rq_bs := range *peer_rq_map[int64(peer_id)] {
+			name, pk, err := dn.ValidateRequest(rq_bs)
+			if err != nil {
+				log.Fatal("%d accepted a bad request: %s", peer_id, err)
+			}
+			if winner_id, already := name_modified[string(name)]; already {
+				log.Printf("Tie over %s between %d and %d broken in favor of %d",
+					string(name), peer_id, winner_id, winner_id)
+				continue
+			}
+			err = mapHandle.Set(merklemap.Hash(name), merklemap.Hash(pk.Bytes))
+			if err != nil {
+				log.Fatalf("mapHandle.Set(name,pk): %s", err)
+			}
+			if _, err = create.Exec(name); err != nil {
+				log.Fatalf("QueueProcessor: create.Exec(name): %s", err)
+			}
+			if _, err = assign.Exec(name, pk.Bytes); err != nil {
+				log.Fatalf("QueueProcessor: create.Exec(name): %s", err)
+			}
+			if _, err = unlock.Exec(name, rq_bs); err != nil {
+				log.Fatalf("QueueProcessor: unlock.Exec(name): %s", err)
+			}
+		}
+	}
+	rootHash, err := mapHandle.GetRootHash()
+	if err != nil {
+		log.Fatalf("mapHandle.GetRootHash(): %s", err)
+	}
+	newNaming, err := mapHandle.FinishUpdate()
+	if err != nil {
+		log.Fatalf("mapHandle.FinishUpdate(): %s", err)
+	}
+	new_snapshot := newNaming.GetId()
+	_ = new_snapshot // TODO: save the new snapshot
+	return rootHash[:]
 }
--- tables.go
+++ tables.go
@@ -7,14 +7,15 @@ func (dn *Dename) CreateTables() {
 	// name_mapping
 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS name_mapping (
 		name bytea not null primary key,
-		pubkey bytea not null);`)
+		pubkey bytea);`)
 	if err != nil {
 		log.Fatal("Cannot create table name_mapping: ", err)
 	}
 
 	// name_locked
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS name_locked (
-		name bytea not null unique primary key);`)
+		name bytea not null unique primary key,
+		request bytea);`)
 	if err != nil {
 		log.Fatal("Cannot create table name_locked: ", err)
 	}
