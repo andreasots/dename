afe13b9abdb515eb3ba90a9b6ddab15eb736bbfa
Daniel Ziegler <dmz@mit.edu>
2014-01-12 19:05:06 -0500

    Revert "wait for all publishes before aggregating"
    This reverts commit 4e81f91a2d20372f78b5df8afb005c3260248048.
    It wasn't actually necessary -- the publishesNext loop waits
    anyway for the publishes to be sent in through publishesIn;
    we might as well start processing them as soon as possible.

--- consensus/round.go
+++ consensus/round.go
@@ -385,7 +385,6 @@ func (r *round) startHandlingPublishes() {
 	// actually chan *sgp.Signed
 	publishesIn := make(chan interface{})
 	publishesNext := make(chan interface{})
-	othersHavePublished := make(chan struct{})
 	go ringchannel.RingIQ(publishesIn, publishesNext)
 	hasPublished := make(map[int64]struct{})
 	r.c.router.Receive(r.id, PUBLISH, func(msg *ConsensusMSG) bool {
@@ -411,12 +410,10 @@ func (r *round) startHandlingPublishes() {
 		done := len(hasPublished) == len(r.c.Peers)-1
 		if done {
 			close(publishesIn)
-			close(othersHavePublished)
 		}
 		return done
 	})
 	go func() {
-		<-othersHavePublished
 		<-r.afterWeHavePublished
 		// Finish verifying & aggregating publishes
 		for item := range publishesNext {
