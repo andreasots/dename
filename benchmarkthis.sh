947f40b84e0b5261d9e12368f74424129d1c0d93
Andres Erbsen <andres@krutt.org>
2014-03-15 16:03:34 -0400

    cleanup

--- benchmarkthis.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-P=900
-regtoken='BPuWwRzl/gp279rMP3qgoAHbMi0bICSxOFJ+fjspJvU='
-
-date '+%s%N' > "bench.start"
-env GOMAXPROCS=80 ../bench $1
-date '+%s%N' > "bench.end"
-
-cat bench.start bench.end
