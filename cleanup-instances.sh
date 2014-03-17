e7e1844946ff4d1c68756cf23545aab246ab52df
Andres Erbsen <andres@krutt.org>
2014-03-16 21:30:10 -0400

    fix cleanup script removing directory too early

--- cleanup-instances.sh
+++ cleanup-instances.sh
@@ -10,14 +10,13 @@ dir="$1"
 count="$2"
 
 cleanup_instance() {
-	rm -rf "$dir/$1"
-	host=$(grep -Pzo '\[general\]\n(.+\n)*host\s*=\s*\K.+' "$dir/$1/dename.cfg")
 	dbname=$(grep -Pzo '\[database\]\n(.+\n)*name\s*=\s*\K.+' "$dir/$1/dename.cfg")
 	dbuser=$(grep -Pzo '\[database\]\n(.+\n)*user\s*=\s*\K.+' "$dir/$1/dename.cfg")
 	echo "
 DROP DATABASE \"$dbname\";
 DROP USER \"$dbuser\";
 \\q" | sudo su - postgres -c psql
+	rm -rf "$dir/$1"
 }
 
 for i in $(seq 1 "$count"); do cleanup_instance "$i"; done
