a7b5961cebcbb53c79fc43970735d9ff3eb20fa7
Andres Erbsen <andres@krutt.org>
2014-03-26 11:21:15 -0400

    network.go:send(): retry and cleanup after fainures

--- network.go
+++ network.go
@@ -193,23 +193,28 @@ func (peer *Peer) receiveLoop(conn net.Conn, cipher cipher.AEAD,
 }
 
 func (peer *Peer) send(tag uint8, msg []byte) error {
-	var nonce uint64
-	var w *authConn
-
-	peer.Lock()
-	for w = range peer.connections {
-		break
-	}
-	if w != nil {
-		nonce = w.nonce
+	for {
+		peer.Lock()
+		if len(peer.connections) == 0 {
+			peer.Unlock()
+			return errors.New(fmt.Sprintf("SendToPeer: No connection to %d", peer.id))
+		}
+		var w *authConn
+		for w = range peer.connections {
+			break
+		}
+		nonce := w.nonce
 		w.nonce++
-	}
-	peer.Unlock()
+		peer.Unlock()
 
-	if w == nil || w.conn == nil {
-		return errors.New(fmt.Sprintf("SendToPeer: No connection to %d", peer.id))
+		err := writeAuth(w.conn, w.cipher, nonce, append(msg, tag))
+		if err == nil {
+			return nil
+		}
+		peer.Lock()
+		delete(peer.connections, w)
+		peer.Unlock()
 	}
-	return writeAuth(w.conn, w.cipher, nonce, append(msg, tag))
 }
 
 func (peer *Peer) ConsensusSend(msg_bs []byte) error {
